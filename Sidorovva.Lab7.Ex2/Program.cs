83c6dddff51da6f1294dbfbf0941197b8d256d84
sidnatasha03 <90253059+sidnatasha03@users.noreply.github.com>
2021-12-15 08:19:15 +0300

    Add files via upload

---
+++ Sidorovva.Lab7.Ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sidorovaa.Lab6.Exercise2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+            Console.WriteLine(str.Where(x => Char.IsDigit(x)).ToArray());
+        
+
+            //string str = Console.ReadLine();
+            //var CharArray = str.ToCharArray();
+            //int a = (int)'0';
+            //int b = (int)'9';
+            //for (var i = 0; i < str.Length; i++)
+            //{
+            //    if (CharArray[i] >= a && CharArray[i] <= b)
+            //    {
+            //        Console.Write(CharArray[i]);
+            //    }
+            //}
+        }
+    }
+}
