7065ad191e0e804414fd2f59729ee9f31c90df96
sidnatasha03 <90253059+sidnatasha03@users.noreply.github.com>
2021-12-19 15:37:30 +0300

    Add files via upload

---
+++ Sidorovvaa.Lab7.Ex1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace Sidorovaa.Lab6.Exercise2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            string s;
+
+            s = Console.ReadLine();
+
+            //количество слов
+            int K;
+            K = 0;
+            bool wrong, space;
+            wrong = false;
+            space = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ')
+                {
+                    space = false;
+
+
+                    if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+                    {
+                        wrong = false;
+                    }
+                    else
+                    {
+                        wrong = true;
+                    }
+                    continue;
+                }
+                else if (wrong == false && space == false)
+                {
+
+                    K++;
+
+                    space = true;
+                    wrong = false;
+                }
+
+                else if (space == false && wrong == false && s.Length > 0)
+                {
+                    K++;
+                }
+            }
+
+
+            Console.WriteLine(K);
+        }
+    }
+}
