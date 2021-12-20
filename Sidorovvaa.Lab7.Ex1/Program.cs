1cfb44ee5982d5d9dff0ef2d26b880789c34288f
sidnatasha03 <90253059+sidnatasha03@users.noreply.github.com>
2021-12-21 02:16:30 +0300

    Update Program.cs

--- Sidorovvaa.Lab7.Ex1/Program.cs
+++ Sidorovvaa.Lab7.Ex1/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Sidorovaa.Lab6.Exercise2
 {
@@ -11,31 +13,63 @@ namespace Sidorovaa.Lab6.Exercise2
 
             string sposob = Console.ReadLine();
 
-            switch (sposob)
-            {
+            switch(sposob)
+                {
 
                 case "1":
 
-                    string str = Console.ReadLine();
-                    Console.WriteLine(str.Where(x => Char.IsDigit(x)).ToArray());
+            string s;
+
+            s = Console.ReadLine();
+
+            int K = 0;
+
+            bool wrong = false, space = false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ')
+                {
+                    space = false;
+
+                    if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+                    {
+                        wrong = false;
+                    }
+                    else
+                    {
+                        wrong = true;
+                    }
+                    continue;
+                }
+                else if (wrong == false && space == false)
+                {
+
+                    K++;
+
+                    space = true;
+                    wrong = false;
+                }
+
+                else if (space == false && wrong == false && s.Length > 0)
+                {
+                    K++;
+                }
+            }
+
+                    Console.WriteLine(K);
+
                     break;
 
                 case "2":
 
-                    
-                    string str1 = Console.ReadLine();
-                    var CharArray = str1.ToCharArray();
-                    int a = (int)'0';
-                    int b = (int)'9';
-                    for (var i = 0; i < str1.Length; i++)
-                    {
-                        if (CharArray[i] >= a && CharArray[i] <= b)
-                        {
-                            Console.Write(CharArray[i]);
-                        }
-                    }
+                    string govno = Console.ReadLine();
+
+                    Console.WriteLine(Regex.Matches(govno, @"[a-zA-Z]+").OfType<Match>().Select(m => m.Value).Count());
+
                     break;
             }
+            
         }
     }
 }
